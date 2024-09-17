76dc91422b60ea5e8fa232703f760443697c9bcb
AdrianMares <adrianmares.dev@gmail.com>
2024-09-17 00:21:48 -0400

    initialized variables

---
+++ main.js
@@ -0,0 +1,11 @@
+let xp = 0;
+let energy = 150;
+let gold = 100;
+
+const btn1 = document.getElementById("btn1");
+const btn2 = document.getElementById("btn2");
+const btn3 = document.getElementById("btn3");
+const description = document.querySelector("#description");
+const energyText = document.getElementById("energyText");
+const goldText = document.getElementById("goldText");
+const xpText = document.getElementById("xpText");
